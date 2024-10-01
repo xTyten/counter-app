4b77cbdca47eed7ecd3ed9635415609210d746fe
xTyten <tylerrenzejan@gmail.com>
2024-10-01 14:13:27 -0400

    first commit

---
+++ src/counter-app.js
@@ -0,0 +1,59 @@
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+
+export class counterApp extends DDDSuper(LitElement) {
+
+  static get tag() {
+    return "counter-app";
+  }
+
+  constructor() {
+    super();
+    this.title = "";
+  }
+
+  static get properties() {
+    return {
+      title: { type: String },
+    };
+  }
+
+  static get styles() {
+    return [super.styles,
+    css`
+      :host {
+        display: block;
+        color: var(--ddd-theme-primary);
+        background-color: var(--ddd-theme-accent);
+        font-family: var(--ddd-font-navigation);
+        font-size: var(--counter-app-font-size, var(--ddd-font-size-s));
+      }
+      .wrapper {
+        margin: var(--ddd-spacing-2);
+        padding: var(--ddd-spacing-4);
+      }
+      div {
+        padding: 0;
+        margin: 0;
+      }
+    `];
+  }
+
+  render() {
+    return html`
+<div class="wrapper">
+  <div>${this.title}</div>
+  <slot></slot>
+</div>`;
+  }
+
+  /**
+   * haxProperties integration via file reference
+   */
+  static get haxProperties() {
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
+      .href;
+  }
+}
+
+globalThis.customElements.define(counterApp.tag, counterApp);
